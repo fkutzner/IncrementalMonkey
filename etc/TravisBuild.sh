12873404c4ac43a74322a74ae2bc1a572c3ec6f3
Felix Kutzner <felixkutzner@gmail.com>
2020-08-30 17:38:23 +0200

    adding Travis build

---
+++ etc/TravisBuild.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+echo "Originally configured C++ compiler: ${CXX}"
+echo "Originally configured C compiler: ${CC}"
+
+host_os=`uname`
+if [[ "${host_os}"  == "Linux" ]] && [[ "${CC}" =~ gcc ]]
+then
+  export CC=gcc-8
+  export CXX=g++-8
+fi
+
+echo "Tool versions:"
+echo "Using C++ compiler: ${CXX}"
+echo "Using C compiler: ${CC}"
+cmake --version
+clang --version
+gcc --version
+echo "(End tool versions)"
+
+build_and_test() {
+  echo "Compilation database:"
+  cat compile_commands.json
+
+  echo "Building..."
+  cmake --build . -- -j2
+
+  echo -n "Testing "
+  bin/incmonktests.libincmonk.unit
+}
+
+BUILD_DIR=$(pwd)
+build_cms() {
+  mkdir cms
+  pushd cms
+  git clone --depth 1 --branch 5.8.0 https://github.com/msoos/cryptominisat
+  pushd cryptominisat
+  git submodule init && git submodule update
+  popd
+  mkdir cms-build
+  cd cms-build
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${BUILD_DIR}/cms/install" ../cryptominisat
+  make -j2 install
+  popd
+}
+
+
+echo "*** Building CryptoMiniSat"
+build_cms
+echo "*** Building IncrementalMonkey"
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH="${BUILD_DIR}/cms/install" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ${TRAVIS_BUILD_DIR}
+build_and_test
