fdfdaf66eab6921ba756ba83574a582b7c9329fc
Felix Kutzner <felixkutzner@gmail.com>
2020-09-15 19:56:25 +0200

    cleaning up IncrementalMonkey.cpp

--- tools/monkey/IncrementalMonkey.cpp
+++ tools/monkey/IncrementalMonkey.cpp
@@ -42,130 +42,285 @@
 
 #include <CLI/CLI.hpp>
 
+#include <optional>
 #include <string>
 
+namespace {
+
+class MonkeyCommand {
+public:
+  // If the command could be executed (ie. was selected by the user),
+  // tryExecute returns the command-main()'s return value. Otherwise,
+  // it returns nothing.
+  virtual auto tryExecute() -> std::optional<int> = 0;
+
+  MonkeyCommand() = default;
+  virtual ~MonkeyCommand() = default;
+
+  MonkeyCommand(MonkeyCommand const&) = delete;
+  auto operator=(MonkeyCommand const&) -> MonkeyCommand& = delete;
+  MonkeyCommand(MonkeyCommand&&) = delete;
+  auto operator=(MonkeyCommand &&) -> MonkeyCommand& = delete;
+};
+
+
+class MonkeyFuzzCommand : public MonkeyCommand {
+public:
+  MonkeyFuzzCommand(CLI::App& app)
+  {
+    m_subApp = app.add_subcommand("fuzz", "Random-test IPASIR libraries");
+    m_subApp->add_option(
+        "--id",
+        m_fuzzerParams.fuzzerId,
+        "Name of the fuzzer instance, included in trace file names (default: random)");
+    m_fuzzMaxRoundsOpt = m_subApp->add_option(
+        "--rounds", m_fuzzMaxRounds, "Number of rounds to be executed (default: no limit)");
+    m_fuzzTimeoutMillisOpt = m_subApp->add_option(
+        "--timeout", m_fuzzTimeoutMillis, "Timeout for solver runs (default: no limit)");
+    m_subApp->add_flag("--no-havoc", m_fuzzerParams.disableHavoc, "Disable havoc commands");
+    m_subApp->add_option(
+        "--seed", m_fuzzerParams.seed, "Random number generator seed for problem generators");
+    m_fuzzCfgFileOpt =
+        m_subApp->add_option("--config",
+                             m_fuzzConfigFile,
+                             "Problem generator configuration file. See print-default-cfg command");
+
+    m_subApp
+        ->add_option(
+            "LIB", m_fuzzerParams.fuzzedLibrary, "Shared library file of the IPASIR solver")
+        ->required();
+  }
+
+  virtual auto tryExecute() -> std::optional<int> override
+  {
+    if (m_subApp->parsed()) {
+      if (!m_fuzzMaxRoundsOpt->empty()) {
+        m_fuzzerParams.roundsLimit = m_fuzzMaxRounds;
+      }
+      if (!m_fuzzTimeoutMillisOpt->empty()) {
+        m_fuzzerParams.timeout = std::chrono::milliseconds{m_fuzzTimeoutMillis};
+      }
+      if (!m_fuzzCfgFileOpt->empty()) {
+        m_fuzzerParams.configFile = m_fuzzConfigFile;
+      }
+      return incmonk::fuzzerMain(m_fuzzerParams);
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  virtual ~MonkeyFuzzCommand() = default;
+
+private:
+  CLI::App* m_subApp = nullptr;
+  CLI::Option* m_fuzzMaxRoundsOpt = nullptr;
+  CLI::Option* m_fuzzTimeoutMillisOpt = nullptr;
+  CLI::Option* m_fuzzCfgFileOpt = nullptr;
+
+  incmonk::FuzzerParams m_fuzzerParams;
+  uint64_t m_fuzzMaxRounds = 0;
+  uint64_t m_fuzzTimeoutMillis = 0;
+  std::filesystem::path m_fuzzConfigFile;
+};
+
+
+class MonkeyReplayCommand : public MonkeyCommand {
+public:
+  MonkeyReplayCommand(CLI::App& app)
+  {
+    m_subApp = app.add_subcommand("replay", "Apply failure traces to IPASIR solvers");
+    m_subApp
+        ->add_option(
+            "LIB", m_replayParams.solverLibrary, "Shared library file of the IPASIR solver")
+        ->required();
+    m_subApp
+        ->add_option("TRACE",
+                     m_replayParams.traceFile,
+                     ".mtr file to apply. If - is specified, the trace is read from the standard "
+                     "input instead.")
+        ->required();
+  }
+
+  virtual auto tryExecute() -> std::optional<int> override
+  {
+    if (m_subApp->parsed()) {
+      return incmonk::replayMain(m_replayParams);
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  virtual ~MonkeyReplayCommand() = default;
+
+private:
+  CLI::App* m_subApp = nullptr;
+  incmonk::ReplayParams m_replayParams;
+};
+
+
+class MonkeyPrintCppCommand : public MonkeyCommand {
+public:
+  MonkeyPrintCppCommand(CLI::App& app)
+  {
+    m_subApp = app.add_subcommand("print-cpp", "Print traces as C++11 code");
+    m_subApp->add_option("--solver-varname",
+                         m_printCppParams.solverVarName,
+                         "Solver variable name (default: solver)");
+    m_subApp->add_option("--function-name",
+                         m_printCppParams.funcName,
+                         "Function name (default: only a body is printed)");
+    m_subApp
+        ->add_option("TRACE",
+                     m_printCppParams.traceFile,
+                     ".mtr file to print. If - is specified, the trace is read from the standard "
+                     "input instead.")
+        ->required();
+  }
+
+  virtual auto tryExecute() -> std::optional<int> override
+  {
+    if (m_subApp->parsed()) {
+      return incmonk::printCPPMain(m_printCppParams);
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  virtual ~MonkeyPrintCppCommand() = default;
+
+private:
+  CLI::App* m_subApp = nullptr;
+  incmonk::PrintCPPParams m_printCppParams;
+};
+
+
+class MonkeyPrintIcnfCommand : public MonkeyCommand {
+public:
+  MonkeyPrintIcnfCommand(CLI::App& app)
+  {
+    m_subApp = app.add_subcommand("print-icnf", "Print traces as ICNF instances");
+    m_subApp
+        ->add_option("TRACE",
+                     m_printIcnfParams.traceFile,
+                     ".mtr file to print. If - is specified, the trace is read from the standard "
+                     "input instead.")
+        ->required();
+  }
+
+  virtual auto tryExecute() -> std::optional<int> override
+  {
+    if (m_subApp->parsed()) {
+      return incmonk::printICNFMain(m_printIcnfParams);
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  virtual ~MonkeyPrintIcnfCommand() = default;
+
+private:
+  CLI::App* m_subApp = nullptr;
+  incmonk::PrintICNFParams m_printIcnfParams;
+};
+
+
+class MonkeyPrintDefaultCfgCommand : public MonkeyCommand {
+public:
+  MonkeyPrintDefaultCfgCommand(CLI::App& app)
+  {
+    m_subApp = app.add_subcommand("print-default-cfg", "Print the default configuration");
+  }
+
+  virtual auto tryExecute() -> std::optional<int> override
+  {
+    if (m_subApp->parsed()) {
+      std::cout << incmonk::getDefaultConfigTOML() << "\n";
+      return EXIT_SUCCESS;
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  virtual ~MonkeyPrintDefaultCfgCommand() = default;
+
+private:
+  CLI::App* m_subApp = nullptr;
+  incmonk::PrintICNFParams m_printIcnfParams;
+};
+
+
+class MonkeyGenTraceCommand : public MonkeyCommand {
+public:
+  MonkeyGenTraceCommand(CLI::App& app)
+  {
+    m_subApp = app.add_subcommand("gen-trace", "Generate a random trace");
+    m_genTraceCfgFileOpt =
+        m_subApp->add_option("--config",
+                             m_fuzzConfigFile,
+                             "Problem generator configuration file. See print-default-cfg command");
+    m_subApp->add_option("OUTPUT", m_genTraceParams.outputFile, "Trace filename")->required();
+    m_subApp->add_option(
+        "--seed", m_genTraceParams.seed, "Random number generator seed for problem generators");
+    m_subApp->add_flag("--no-havoc", m_genTraceParams.disableHavoc, "Disable havoc commands");
+
+    m_subApp
+        ->add_option("--generator", m_genTraceParams.generator, "Select generator. Default: cam")
+        ->transform(CLI::IsMember({"cam", "simp-para"}))
+        ->default_val("cam");
+  }
+
+  virtual auto tryExecute() -> std::optional<int> override
+  {
+    if (m_subApp->parsed()) {
+      if (!m_genTraceCfgFileOpt->empty()) {
+        m_genTraceParams.configFile = m_fuzzConfigFile;
+      }
+      return incmonk::genTraceMain(m_genTraceParams);
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  virtual ~MonkeyGenTraceCommand() = default;
+
+private:
+  CLI::App* m_subApp = nullptr;
+  CLI::Option* m_genTraceCfgFileOpt = nullptr;
+
+  incmonk::GenTraceParams m_genTraceParams;
+  std::filesystem::path m_fuzzConfigFile;
+};
+
+}
 
 auto main(int argc, char** argv) -> int
 {
-  incmonk::FuzzerParams fuzzerParams;
-  incmonk::ReplayParams replayParams;
-  incmonk::PrintCPPParams printParams;
-  incmonk::PrintICNFParams printICNFParams;
-  incmonk::GenTraceParams genTraceParams;
-
-  uint64_t fuzzMaxRounds = 0;
-  uint64_t fuzzTimeoutMillis = 0;
-  std::filesystem::path fuzzConfigFile;
-
   std::string const version = INCMONK_VERSION;
   CLI::App app{"A random-testing tool for IPASIR implementations\nVersion " + version, "monkey"};
-  CLI::App* fuzzApp = app.add_subcommand("fuzz", "Random-test IPASIR libraries");
-  fuzzApp->add_option(
-      "--id",
-      fuzzerParams.fuzzerId,
-      "Name of the fuzzer instance, included in trace file names (default: random)");
-  CLI::Option* fuzzMaxRoundsOpt = fuzzApp->add_option(
-      "--rounds", fuzzMaxRounds, "Number of rounds to be executed (default: no limit)");
-  CLI::Option* fuzzTimeoutMillisOpt = fuzzApp->add_option(
-      "--timeout", fuzzTimeoutMillis, "Timeout for solver runs (default: no limit)");
-  fuzzApp->add_flag("--no-havoc", fuzzerParams.disableHavoc, "Disable havoc commands");
-  fuzzApp->add_option(
-      "--seed", fuzzerParams.seed, "Random number generator seed for problem generators");
-  CLI::Option* fuzzCfgFileOpt =
-      fuzzApp->add_option("--config",
-                          fuzzConfigFile,
-                          "Problem generator configuration file. See print-default-cfg command");
 
-  fuzzApp->add_option("LIB", fuzzerParams.fuzzedLibrary, "Shared library file of the IPASIR solver")
-      ->required();
-
-
-  CLI::App* replayApp = app.add_subcommand("replay", "Apply failure traces to IPASIR solvers");
-  replayApp
-      ->add_option("LIB", replayParams.solverLibrary, "Shared library file of the IPASIR solver")
-      ->required();
-  replayApp
-      ->add_option("TRACE",
-                   replayParams.traceFile,
-                   ".mtr file to apply. If - is specified, the trace is read from the standard "
-                   "input instead.")
-      ->required();
-
-
-  CLI::App* printCPPApp = app.add_subcommand("print-cpp", "Print traces as C++11 code");
-  printCPPApp->add_option(
-      "--solver-varname", printParams.solverVarName, "Solver variable name (default: solver)");
-  printCPPApp->add_option(
-      "--function-name", printParams.funcName, "Function name (default: only a body is printed)");
-  printCPPApp
-      ->add_option("TRACE",
-                   printParams.traceFile,
-                   ".mtr file to print. If - is specified, the trace is read from the standard "
-                   "input instead.")
-      ->required();
-
-  CLI::App* printIcnfApp = app.add_subcommand("print-icnf", "Print traces as ICNF instances");
-  printIcnfApp
-      ->add_option("TRACE",
-                   printICNFParams.traceFile,
-                   ".mtr file to print. If - is specified, the trace is read from the standard "
-                   "input instead.")
-      ->required();
-
-  CLI::App* printDefaultCfgApp =
-      app.add_subcommand("print-default-cfg", "Print the default configuration");
-
-
-  CLI::App* genTraceApp = app.add_subcommand("gen-trace", "Generate a random trace");
-  CLI::Option* genTraceCfgFileOpt = genTraceApp->add_option(
-      "--config",
-      fuzzConfigFile,
-      "Problem generator configuration file. See print-default-cfg command");
-  genTraceApp->add_option("OUTPUT", genTraceParams.outputFile, "Trace filename")->required();
-  genTraceApp->add_option(
-      "--seed", genTraceParams.seed, "Random number generator seed for problem generators");
-  genTraceApp->add_flag("--no-havoc", genTraceParams.disableHavoc, "Disable havoc commands");
-
-  genTraceApp->add_option("--generator", genTraceParams.generator, "Select generator. Default: cam")
-      ->transform(CLI::IsMember({"cam", "simp-para"}))
-      ->default_val("cam");
-
+  std::vector<std::unique_ptr<MonkeyCommand>> commands;
+  commands.emplace_back(std::make_unique<MonkeyFuzzCommand>(app));
+  commands.emplace_back(std::make_unique<MonkeyGenTraceCommand>(app));
+  commands.emplace_back(std::make_unique<MonkeyPrintCppCommand>(app));
+  commands.emplace_back(std::make_unique<MonkeyPrintDefaultCfgCommand>(app));
+  commands.emplace_back(std::make_unique<MonkeyPrintIcnfCommand>(app));
+  commands.emplace_back(std::make_unique<MonkeyReplayCommand>(app));
 
   app.require_subcommand(1);
   CLI11_PARSE(app, argc, argv);
 
-  if (fuzzApp->parsed()) {
-    if (!fuzzMaxRoundsOpt->empty()) {
-      fuzzerParams.roundsLimit = fuzzMaxRounds;
+  for (auto const& cmd : commands) {
+    if (std::optional<int> result = cmd->tryExecute(); result.has_value()) {
+      return *result;
     }
-    if (!fuzzTimeoutMillisOpt->empty()) {
-      fuzzerParams.timeout = std::chrono::milliseconds{fuzzTimeoutMillis};
-    }
-    if (!fuzzCfgFileOpt->empty()) {
-      fuzzerParams.configFile = fuzzConfigFile;
-    }
-    return incmonk::fuzzerMain(fuzzerParams);
-  }
-  else if (replayApp->parsed()) {
-    return incmonk::replayMain(replayParams);
-  }
-  else if (printCPPApp->parsed()) {
-    return incmonk::printCPPMain(printParams);
-  }
-  else if (printIcnfApp->parsed()) {
-    return incmonk::printICNFMain(printICNFParams);
-  }
-  else if (printDefaultCfgApp->parsed()) {
-    std::cout << incmonk::getDefaultConfigTOML() << "\n";
-    return EXIT_SUCCESS;
-  }
-  else if (genTraceApp->parsed()) {
-    if (!genTraceCfgFileOpt->empty()) {
-      genTraceParams.configFile = fuzzConfigFile;
-    }
-    return incmonk::genTraceMain(genTraceParams);
   }
 
-  // Not reachable
+  // Not reachable: exactly one command returns a non-empty result
   return EXIT_FAILURE;
 }
