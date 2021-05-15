c49f15bafaad675ac2e303679ce2361af35689c0
Felix Kutzner <felixkutzner@gmail.com>
2021-05-15 19:05:08 +0200

    trying to fix Travis build

--- etc/TravisBuild.sh
+++ etc/TravisBuild.sh
@@ -8,8 +8,8 @@ echo "Originally configured C compiler: ${CC}"
 host_os=`uname`
 if [[ "${host_os}"  == "Linux" ]] && [[ "${CC}" =~ clang ]]
 then
-  export CC=clang-9
-  export CXX=clang++-9
+  export CC=clang-10
+  export CXX=clang++-10
 fi
 
 echo "Tool versions:"
@@ -20,35 +20,7 @@ clang --version
 gcc --version
 echo "(End tool versions)"
 
-build_and_test() {
-  echo "Compilation database:"
-  cat compile_commands.json
+# Run the CMake superbuild. This will also execute the unit tests.
+cmake -DCMAKE_BUILD_TYPE=Release ${TRAVIS_BUILD_DIR}/buildscript
+cmake --build . -- -j2
 
-  echo "Building..."
-  cmake --build . -- -j2
-
-  echo -n "Testing "
-  ctest --output-on-failure
-}
-
-BUILD_DIR=$(pwd)
-build_cms() {
-  mkdir cms
-  pushd cms
-  git clone --depth 1 --branch 5.8.0 https://github.com/msoos/cryptominisat
-  pushd cryptominisat
-  git submodule init && git submodule update
-  popd
-  mkdir cms-build
-  cd cms-build
-  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${BUILD_DIR}/cms/install" ../cryptominisat
-  make -j2 install
-  popd
-}
-
-
-echo "*** Building CryptoMiniSat"
-build_cms
-echo "*** Building IncrementalMonkey"
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH="${BUILD_DIR}/cms/install" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ${TRAVIS_BUILD_DIR}
-build_and_test
